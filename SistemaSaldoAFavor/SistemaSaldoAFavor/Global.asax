f8e3f9f1ac6d009ad3f5f5c3e792d9ed82e8d260
jose.encarnacion <jose.encarnacion@LPT-INF-276>
2018-05-07 11:00:15 -0400

    commit all

---
+++ SistemaSaldoAFavor/SistemaSaldoAFavor/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SistemaSaldoAFavor.MvcApplication" Language="C#" %>
---
+++ SistemaSaldoAFavor/SistemaSaldoAFavor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SistemaSaldoAFavor.Startup))]
+namespace SistemaSaldoAFavor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
